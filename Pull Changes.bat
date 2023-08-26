4791a2bbd13e8ed7e9a78a27ca9a8ee152490241
Hussain-Ali-Al-Talaq <85634202+Hussain-Ali-Al-Talaq@users.noreply.github.com>
2023-08-26 18:53:35 +0300

    Update Pull Changes.bat

--- Pull Changes.bat
+++ Pull Changes.bat
@@ -1,2 +1,4 @@
-git pull https://github.com/Hussain-Ali-Al-Talaq/Mini-Party-Dev.git main --allow-unrelated-histories --no-commit --force    
+git pull https://github.com/Hussain-Ali-Al-Talaq/Mini-Party-Dev.git main --allow-unrelated-histories --no-commit
+git add -A 
+git merge --continue
 PAUSE
